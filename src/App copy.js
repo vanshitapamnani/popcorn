3f18e711569a17309ce8936e83f06420abb5295d
vanshita <pamnanivanshita@gmail.com>
2026-03-07 00:50:21 +0530

    input field modifying

--- src/App copy.js
+++ src/App copy.js
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState, useRef } from "react";
 import StarRating from "./starRating";
 
 // const tempMovieData = [
@@ -243,6 +243,23 @@ function Logo() {
   );
 }
 function Search({ query, setQuery }) {
+  //REF
+  const inputEl = useRef(null);
+
+  useEffect(
+    function () {
+      function callback(e) {
+        if (document.activeElement === inputEl.current) return;
+        if (e.code === "Enter") {
+          inputEl.current.focus();
+          setQuery("");
+        }
+      }
+      document.addEventListener("keydown", callback);
+      return () => document.addEventListener("keydown", callback);
+    },
+    [setQuery],
+  );
   return (
     <input
       className="search"
@@ -250,6 +267,7 @@ function Search({ query, setQuery }) {
       placeholder="Search movies..."
       value={query}
       onChange={(e) => setQuery(e.target.value)}
+      ref={inputEl}
     />
   );
 }
